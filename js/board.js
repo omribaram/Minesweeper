df0e70ffbea964d6d2c2f5fda2c8d9d1129a038f
omribaram <84462496+omribaram@users.noreply.github.com>
2021-05-20 22:27:46 +0300

    Add files via upload

--- js/board.js
+++ js/board.js
@@ -1,6 +1,8 @@
 'use strict'
 
 var gBoard;
+var gIsManualMine;
+var gStopwatchInterval;
 
 function buildBoard() {
     gBoard = [];
@@ -47,6 +49,82 @@ function renderCell(op, el1, el2, clsName1, clsName2) {
                         currCell.innerHTML = `<span class="content">${gBoard[i][j].minesAroundCount}</span>\n<span class="flagged">${FLAG}</span>\n`
                         break;
                 }
+            }
+        }
+    }
+}
+
+function minesPlacement(elCell, i, j) {
+    if (gIsManualMine) {
+        if (gManualMines.length <= gLevel.MINES) {
+            // check to see if already mined
+            if (gManualMines.findIndex(idx => (idx.i === i) && (idx.j === j)) === -1) gManualMines.push({ i: i, j: j });
+            else return
+            BTN_MANUALMINES.classList.add('active');
+            renderCell('add', elCell, '', 'shown-manual-mine', '');
+            BTN_MANUALMINES.childNodes[1].innerText = gLevel.MINES - gManualMines.length;
+            setTimeout(() => {
+                renderCell('remove', elCell, '', 'shown-manual-mine', '');
+            }, 500);
+            if (gManualMines.length === gLevel.MINES) {
+                placeMinesManually();
+                setMinesNegsCount(gBoard); // update matrix with mines negs count
+                renderCell('html', '', '', '', '');
+                startStopwatch();
+                gGame.firstClick = false;
+                BTN_MANUALMINES.childNodes[1].innerText = '0';
+                BTN_MANUALMINES.disabled = true;
+                return false
+            } else return false
+        }
+    } else {
+        placeMinesAuto({ i: i, j: j }); // place mines randomly
+        setMinesNegsCount(gBoard); // update matrix with mines negs count
+        renderCell('html', '', '', '', '');
+        startStopwatch();
+        gGame.firstClick = false;
+        BTN_MANUALMINES.disabled = true;
+        return true
+    }
+}
+
+// Place mines on board randomly as per the amount of defined gLevel
+// If there's a mine at location already, repeats...
+function placeMinesAuto(frstClickIdx) {
+    var loc;
+    var count = 1;
+    while (count <= gLevel.MINES) {
+        loc = { i: getRandomInt(0, gBoard.length - 1), j: getRandomInt(0, gBoard[0].length - 1) };
+        if ((frstClickIdx) && (loc.i === frstClickIdx.i && loc.j === frstClickIdx.j) || (gBoard[loc.i][loc.j].isMine)) continue;
+        gBoard[loc.i][loc.j].isMine = true;
+        count++;
+    }
+}
+
+function placeMinesManually() {
+    if (!gGame.firstClick) return;
+    if (!gIsManualMine) gIsManualMine = true;
+    else {
+        for (var idx = gManualMines.length; idx > 0; idx--) {
+            var mineIdx = gManualMines[idx - 1];
+            gBoard[mineIdx.i][mineIdx.j].isMine = true;
+            gManualMines.pop();
+        }
+        gIsManualMine = false;
+    }
+}
+
+function setMinesNegsCount(board) {
+    var negsMinesCount;
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board[0].length; j++) {
+            negsMinesCount = 0;
+            if (!board[i][j].isMine) {
+                var negs = getNegs({ i: i, j: j }); // Gets the negs of checked idx and iterates through it
+                for (var idx = 0; idx < negs.length; idx++) {
+                    if (board[negs[idx].i][negs[idx].j].isMine) negsMinesCount++;
+                }
+                board[i][j].minesAroundCount = negsMinesCount;
             }
         }
     }
